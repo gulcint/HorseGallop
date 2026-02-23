1c6183e53eaccd079463c25f749313e3d3d6b14d
gulcint <contact@gulcint.com.tr>
2026-02-23 23:37:16 +0300

    fix: use direct firebase functions dependency in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.firestore)
-    implementation(libs.firebase.functions)
+    implementation("com.google.firebase:firebase-functions-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation(libs.firebase.appcheck.playintegrity)
     implementation(libs.firebase.appcheck.debug)
